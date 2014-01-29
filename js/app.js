a34d56f154ebeac6f9a123320be60e81b94ff2f7
Jan Milosh <janmilosh@gmail.com>
2014-01-29 06:49:06 -0500

    Adds routing functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngRoute', 'ngSanitize'])
 
 .provider('Weather', function() {
 	var apiKey = "";
-
-	this.setApiKey = function(key) {
-		if (key) this.apiKey = key;
-	};
 	this.getUrl = function(type, ext) {
 		return "http://api.wunderground.com/api/" + 
 			this.apiKey + "/" + type + "/q/" +
 			ext + '.json';
 	};
+	this.setApiKey = function(key) {
+		if (key) this.apiKey = key;
+	};
+	
 	this.$get = function($q, $http) {
 		var self = this;
 		return {
@@ -34,7 +34,18 @@ angular.module('myApp', [])
 .config(function(WeatherProvider) {
 	WeatherProvider.setApiKey('daea4dbf8e2953aa');
 })
-
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'MainCtrl'
+	})
+	.when('/settings', {
+		templateUrl: 'templates/settings.html',
+		controller: 'SettingsCtrl'
+	})
+	.otherwise({redirectTo: '/'});
+}])
 .controller('MainCtrl', function($scope, $timeout, Weather) {
 
 	$scope.date = {};
